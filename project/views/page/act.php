1dd1a52442c312b8b15a30cd9cacfcea4d29f539
Oleg.Laptev <olegpal55@gmail.com>
2022-04-26 14:12:19 +0300

    z95.3

--- project/views/page/act.php
+++ project/views/page/act.php
@@ -7,13 +7,16 @@
  <body>
 <div>
 	это представление
-	действия act контроллера page
 	действия act контроллера
 </div>
+<h1><?= $header ?></h1>
 <ul>
 	<li><?php echo $var1; ?></li>
 	<li><?php echo $var2; ?></li>
 	<li><?php echo $var3; ?></li>
+	<?php foreach ($users as $user): ?>
+		<li><?= $user; ?></li>
+	<?php endforeach; ?>
 </ul>
  </body>
 </html>
