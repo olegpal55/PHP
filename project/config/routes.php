0ce09268c5a53534eb6a232d4296f19ec88fbbd3
Oleg.Laptev <olegpal55@gmail.com>
2022-04-26 13:52:55 +0300

    z94.1

--- project/config/routes.php
+++ project/config/routes.php
@@ -1,13 +1,15 @@
 <?php
 	use \Core\Route;
+
 	return [
-		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
 		new Route('/my-page1/', 'page', 'show1'),
 		new Route('/my-page2/', 'page', 'show2'),
-		new Route('/p/', 'test', 'act1'),
-		new Route('/pok/', 'test', 'act2'),
-		new Route('/k/', 'test', 'act3'),
-
+		new Route('/privet/', 'test', 'act1'),
+		new Route('/poka/', 'test', 'act2'),
+		new Route('/kak/', 'test', 'act3'),
+		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
+		new Route('/user/:id/', 'user', 'show'),
+		new Route('/user/:id/:key/', 'user', 'info'),
+		new Route('/page/act/', 'page', 'act'),
 	];
-
-	?>
+?>
