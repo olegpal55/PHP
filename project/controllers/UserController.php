4f60351cb74029d440f1c3767a912cbd06480372
Oleg.Laptev <olegpal55@gmail.com>
2022-04-26 14:11:27 +0300

    z95.2

--- project/controllers/UserController.php
+++ project/controllers/UserController.php
@@ -1,8 +1,16 @@
 <?php
     namespace Project\Controllers;
     use Core\Controller;
+namespace Project\Controllers;
+use Core\Controller;
 
     class UserController extends Controller
+class UserController extends Controller
+{
+    private $users;
+
+
+    public function __construct()
     {
         private $users;
 
@@ -14,12 +22,45 @@
         3 => ['name'=>'user3', 'age'=>'25', 'salary' => 3000],
         4 => ['name'=>'user4', 'age'=>'26', 'salary' => 4000],
         5 => ['name'=>'user5', 'age'=>'27', 'salary' => 5000],
+        $this->users = [
+            1 => ['name'=>'user1', 'age'=>'23', 'salary' => 1000],
+            2 => ['name'=>'user2', 'age'=>'24', 'salary' => 2000],
+            3 => ['name'=>'user3', 'age'=>'25', 'salary' => 3000],
+            4 => ['name'=>'user4', 'age'=>'26', 'salary' => 4000],
+            5 => ['name'=>'user5', 'age'=>'27', 'salary' => 5000],
         ];
         }
+    }
+    public function show($params)
+    {
+            print_r( $this->users[ $params['id']]); // âûâîäèì ñòðàíèöó ïî íîìåðó
+    }
+    public function info($params){
+        echo $this->users[$params['id']]['name'];
+    }
 
         public function show($params)
         {
             var_export ($this->users[ $params['id'] ]);
+    public function all(){
+        $i=1;
+        while ($i<5){
+            print_r ($this->users[$i]);
+            echo '<p></p>';
+            $i++;
         }
     }
+
+    public function first($params){
+        //?!
+        $i=1;
+        $p=(int)$params;
+        while ($i<$p){
+            print_r ($this->users[$i]);
+            echo '<p></p>';
+            $i++;
+        }
+        }
+
+}
 ?>
--- project/views/page/act.php
+++ project/views/page/act.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
  <head>
   <meta charset="utf-8" />
@@ -5,8 +6,14 @@
  </head>
  <body>
 <div>
-    это представление
-    действия act контроллера page
+	это представление
+	действия act контроллера page
+	действия act контроллера
 </div>
+<ul>
+	<li><?php echo $var1; ?></li>
+	<li><?php echo $var2; ?></li>
+	<li><?php echo $var3; ?></li>
+</ul>
  </body>
 </html>
